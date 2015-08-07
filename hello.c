8ff0eb141d06c52546f349aa38144576a9bced06
ace_one <1506155477@qq.com>
2015-08-07 10:28:53 +0800

    AAAAAAAAAAAAAAAAA

--- hello.c
+++ hello.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-hahahahhdjfhajsdhjfhajshdkj
-dfasdfjahsjdjh
-
-
-
-jdfhjasdhjkfaks
-int main()
-{
-	printf("Hello world");
-	return 0jfhjasdhfja;
-	printf("jfahsjdfhaweryh");
-}
-//heooiojmnup heyouppp and hei
-//kkkkk
-
-my name is heyoufei
-add something
+test test
